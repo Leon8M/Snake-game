31aa015e12c94ebd270dcbc18b60b7ab9d8fc62f
Leon Munene <leonmunene@gmail.com>
2024-11-11 15:37:26 +0300

    updt

--- src/snake.rs
+++ src/snake.rs
@@ -5,7 +5,7 @@ use piston_window::types::Color;
 use crate::draw::draw_block;
 
 const SNAKE_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum Direction {
     Up,
     Down,
@@ -57,7 +57,7 @@ impl Snake {
         }
     }
 
-    pub fn draw(&self, con: &mut Context, g: &mut G2d){
+    pub fn draw(&self, con: &Context, g: &mut G2d){
         for block in &self.body{
             draw_block(SNAKE_COLOR, block.x,block.y, con, g);
         }
@@ -102,5 +102,42 @@ impl Snake {
 
     pub fn head_direction(&self) -> Direction{
         self.direction
+    }
+
+    pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32) {
+        let (head_x, head_y): (i32, i32) = self.head_position();
+
+        let mut moving_dir = self.direction;
+        match dir {
+            Some(d) => moving_dir = d,
+            None => {}
+        }
+
+        match moving_dir {
+            Direction::Up => (head_x, head_y - 1),
+            Direction::Down => (head_x, head_y + 1),
+            Direction::Left => (head_x - 1, head_y),
+            Direction::Right => (head_x + 1, head_y),
+        }
+    }
+
+    pub fn restore_tail(&mut self) {
+        let blk = self.tail.clone().unwrap();
+        self.body.push_back(blk);
+    }
+
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+        let mut ch = 0;
+        for block in &self.body {
+            if block.x == x && block.y == y {
+                return true;
+            }
+
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+        return  false;
     }
 }
